929464c5c25996bced782638c3da45bd5b8b01cf
Firdaus Adi Putra <mail@firdaus.org>
2023-08-08 00:12:31 +0700

    Implement validator

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
+const { validate } = require('../middlewares/validate');
+const { registrationRules } = require('../modules/controller/api_request');
+
 const router = express.Router();
 
 module.exports = (apiController) => {
-  router.post('/v1/register', (req, res, next) => apiController.register(req, res, next));
+  router.post('/v1/register', registrationRules(), validate, (req, res, next) => apiController.register(req, res, next));
 
   return router;
 };
