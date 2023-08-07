710354b8b863f2f49fb3c6d4b6cf5a4bc260cb7c
Firdaus Adi Putra <mail@firdaus.org>
2023-08-07 23:31:02 +0700

    Hash password

--- modules/user/service.js
+++ modules/user/service.js
@@ -1,11 +1,32 @@
+const bcrypt = require('bcrypt');
+const { v4: uuidv4 } = require('uuid');
+
+
 class UserService {
   constructor(userRepository) {
     this.userRepository = userRepository;
   }
 
   async create(data) {
-    return this.userRepository.create(data);
+    // hash password
+    const hashedPassword = await hashPassword(data.password);
+
+    const params = {
+      ...data,
+      password: hashedPassword,
+    }
+    return this.userRepository.create(params);
   }
 }
+
+const hashPassword = async (password) => {
+  try {
+    const saltRounds = 12;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
+  } catch (error) {
+    throw new Error('Failed to hash password');
+  }
+};
 
 module.exports = UserService;
